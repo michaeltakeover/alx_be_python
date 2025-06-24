7dd890ec532034de8f0892a413b6eed9a8ba9dee
Osayande <michaeltakeover@gmail.com>
2025-06-24 11:29:37 +0100

    adding future age calculator

---
+++ python_indroduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+# FUTURE_AGE_CALCULATOR.PY
+current_age = int(input("How old are you? "))
+current_year = 2023
+in_2050 = 2050
+future_age = in_2050 - current_year + current_age
+
+print(f"In 2050, you will be {future_age} years old.")
---
+++ python_indroduction/hours_to_seconds.py
@@ -0,0 +1,5 @@
+# HOURS_TO_SECONDS.PY
+hours = 2
+sec_conversion = 3600
+seconds = hours * sec_conversion
+print(hours, "hour(s) is", seconds, "seconds")
