5946f8ae8c9dbc9037f96d4a2dc998440e196751
Osayande <michaeltakeover@gmail.com>
2025-07-02 19:02:57 +0100

    updating

--- control_flow/pattern_drawing.py
+++
@@ -1,8 +0,0 @@
-size = int(input("Enter the size of the pattern: "))
-
-row = 1
-while row <= size:
-    for col in range(size):
-        print("*", end="")  # print stars side by side
-    print()  # move to the next line after each row
-    row += 1
