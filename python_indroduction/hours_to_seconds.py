588eb80e44fad15aeb05d491f265f54c10bb4942
Amiegbe Osayande Michael <141160325+michaeltakeover@users.noreply.github.com>
2025-07-02 18:53:31 +0100

    Delete python_indroduction directory

--- python_indroduction/hours_to_seconds.py
+++
@@ -1,5 +0,0 @@
-# HOURS_TO_SECONDS.PY
-hours = 2
-sec_conversion = 3600
-seconds = hours * sec_conversion
-print(hours, "hour(s) is", seconds, "seconds")
--- python_indroduction/rectangle_area.py
+++
@@ -1,5 +0,0 @@
-# RECTANGLE_AREA.PY
-length = 10
-width = 5
-area = length * width 
-print("The ares of the rectangle is:", area)
