e49c291a68274b14286a7d8911b2232234aaf97a
Osayande <michaeltakeover@gmail.com>
2025-07-29 04:03:29 +0100

    updated the simple interest

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,13 @@
+'''python .\main.py
+Arithmetic Operations
+Enter the first number: 5
+Enter the second number: 6
+Enter the operation (add, subtract, multiply, divide): add
+Result: 11.0'''
+# arithmetic_operations.py
+def Arithmetic_Operation():
+    print("Arithmetic Operations")
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the second number: "))
+    #print(f"add = {num1 + num2}")
+    
--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,6 +1,6 @@
-#SIMPLE INTWREST
+cl#SIMPLE INTWREST
 principal = 1000
 rate = 0.05
 time = 3
-interest = (principal * rate * time)
+interest = principal * rate * time
 print(f"The simple interest is: {interest}")
