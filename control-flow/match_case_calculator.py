fce84d05cd302967c62586d3cb678c08ad8eab0b
Osayande <michaeltakeover@gmail.com>
2025-07-30 13:27:11 +0100

    update

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,7 +4,7 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
-user_choice = input("Choose the operation, (+, -, *, /): ")
+user_choice = input("Choose the operation (+, -, *, /): ")
 
 match user_choice:
     case "+":
@@ -14,7 +14,7 @@ match user_choice:
     case "*":
         print(f"The result is {num1 * num2}")
     case "/":
-        if num2 == 0:
+        if num2 != 0:
             print(f"The result is {num1 / num2}")
         else:
             print("you cannot divide by zero")
