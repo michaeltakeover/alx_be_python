81f9a4ccaa50ea51fcdd841501324d6b790ffcbe
Osayande <michaeltakeover@gmail.com>
2025-08-07 04:29:19 +0100

    updating fns

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -5,7 +5,7 @@ def perform_operation(num1, num2, operation:str):
     elif operation == "subtract":
         return num1 -  num2
     elif operation == "multiply":
-        return num1 * num2
+        return num1 * num2 * operation
     elif operation == "divide":
         if num2 == 0:
             return "Error: ZeroDivisionError"
