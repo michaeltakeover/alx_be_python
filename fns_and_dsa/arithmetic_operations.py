5da3ffee6dbcb24502123cc4662c0e9149a56621
Osayande <michaeltakeover@gmail.com>
2025-08-07 04:11:11 +0100

    updating fns

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,16 +1,4 @@
-'''python .\main.py
-Arithmetic Operations
-Enter the first number: 5
-Enter the second number: 6
-Enter the operation (add, subtract, multiply, divide): add
-Result: 11.0
-# arithmetic_operations.py
-def Performe_Operation():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    #print(f"add = {num1 + num2}")
-    '''
+
 def perform_operation(num1:float, num2:float, operation:str):
     if operation == "add":
         return num1 + num2
@@ -21,5 +9,6 @@ def perform_operation(num1:float, num2:float, operation:str):
     elif operation == "divide":
         if num2 == 0:
             return "" "Error: ZeroDivisionError"
+        return num1 / num2
     else:
         return "Invalid Input"
