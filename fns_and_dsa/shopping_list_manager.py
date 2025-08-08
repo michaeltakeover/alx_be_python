cf7f45a417f45d7faf563219b55bf6559cdc4491
Osayande <michaeltakeover@gmail.com>
2025-08-08 02:14:09 +0100

    updating fns and dsa

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,34 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # Prompt for and add an item
+            add = input("Enter an item to add: ").lower()
+            pass
+        elif choice == '2':
+            # Prompt for and remove an item
+            remove = input("choose an item to remove from the list: ").lower()
+            if remove in shopping_list:
+                shopping_list.remove(remove)
+            else:
+                print("your input is invalid")
+        elif choice == '3':
+            # Display the shopping list
+            print(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
